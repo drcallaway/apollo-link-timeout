c325c77324f3fb737c6cdc1f77e1ce7246fba77d
Dustin Callaway <dustin_callaway@intuit.com>
2018-08-20 16:22:04 -0600

    move tests directory

--- __tests__/timeoutLink.test.ts
+++ __tests__/timeoutLink.test.ts
@@ -1,4 +1,4 @@
-import TimeoutLink from '../timeoutLink';
+import TimeoutLink from '../src/timeoutLink';
 import { ApolloLink, execute, Observable } from 'apollo-link';
 import gql from 'graphql-tag';
 
