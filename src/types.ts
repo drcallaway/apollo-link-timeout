e9b16609f0f0290bf462d993271ea168bc43b0ee
Matthew Peveler <matt.peveler@gmail.com>
2025-11-29 22:44:32 -0700

    remove @ts-ignore
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,14 @@
-// @ts-ignore
 import { ApolloLink } from '@apollo/client/core';
 
-// @ts-ignore
 export type Operation = ApolloLink.Operation extends never
   ? import('@apollo/client/core').Operation
-  // @ts-ignore
   : ApolloLink.Operation;
 
-// @ts-ignore
 export type NextLink = ApolloLink.ForwardFunction extends never
   // @ts-ignore
   ? import('@apollo/client/core').NextLink
-  // @ts-ignore
   : ApolloLink.ForwardFunction;
 
-// @ts-ignore
 export type FetchResult<T = any> = ApolloLink.Result extends never
   ? import('@apollo/client/core').FetchResult<T>
-  // @ts-ignore
   : ApolloLink.Result<T>;
